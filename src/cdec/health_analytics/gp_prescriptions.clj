7c556769af0eda5d5b196152571378b57e9b73da
Bruce Durling <bld@otfrom.com>
2013-07-12 20:09:44 +0100

    First cut of diabetes drugs.

--- src/cdec/health_analytics/gp_prescriptions.clj
+++ src/cdec/health_analytics/gp_prescriptions.clj
@@ -13,16 +13,64 @@
   (<- [?sha ?pct ?practice ?bnf_code ?bnf_name ?items ?nic ?act_cost ?quantity ?period]
       (in :> ?sha ?pct ?practice ?bnf_code ?bnf_name ?items ?nic ?act_cost ?quantity ?period _)))
 
-(defn contains-string? [src search-term]
+(defn contains-string? [search-term src]
   (< -1 (.indexOf (string/lower-case src) (string/lower-case search-term))))
+
+(defn in? [src terms]
+  (re-find (re-pattern (clojure.string/join \| terms)) src))
+
+(defn diabetes-drug? [bnf-name]
+  (in? bnf-name [;; short acting insulin
+                 "Humalog"  "Actrapid" "Humulin" "NovoRapid" "Apidra"
+                 ;; intermediate and long-acting insulins
+                 "Insulatard" "Lantus" "Levemir" "Mixtard" "Insuman" "NovoMix"
+                 ;; Sulphonyleureas
+                 "Gliclazide" "Glimepiride" "Tolbutamide"
+                 ;; Biguanides
+                 "Metformin"
+                 ;; Repaglinide & Nateglinide
+                 "Nateglinide" "Repaglinide"
+                 ;; Thiazolidinediones (Glitazones)
+                 "Pioglitazone" "Acarbose" "Sitagliptin" "Linagliptin"
+                 ;; Exenatide
+                 "Exenatide"
+                 ;; Liraglutide
+                 "Liraglutide"
+                 ;; mypoglyceaemia
+                 "Glucagon"
+                 ;; kit
+                 "Lancet" "Strips"]))
 
 (defn humalog [scrips]
   (<- [?sha ?pct ?practice ?bnf-code ?bnf-name ?items ?nic ?act-cost ?quantity ?period]
       (scrips :> ?sha ?pct ?practice ?bnf-code ?bnf-name ?items ?nic ?act-cost ?quantity ?period)
-      (contains-string? ?bnf-name "Humalog")))
+      (contains-string? "Humalog" ?bnf-name)))
 
 #_(?- (hfs-delimited "./output/humalog/" :delimiter "," :sinkmode :replace)
       (humalog
+       (gp-prescriptions
+        (hfs-delimited "./input/prescriptions/pdpi" :delimiter ",")))
+      (:trap (stdout)))
+
+(defn diabetes-scrips [scrips]
+  (<- [?sha ?pct ?practice ?bnf-code ?bnf-name ?items ?nic ?act-cost ?quantity ?period]
+      (scrips :> ?sha ?pct ?practice ?bnf-code ?bnf-name ?items ?nic ?act-cost ?quantity ?period)
+      (diabetes-drug? ?bnf-name)))
+
+#_(?- (hfs-delimited "./output/humalog/" :delimiter "," :sinkmode :replace)
+      (diabetes-scrips
+       (gp-prescriptions
+        (hfs-delimited "./input/prescriptions/pdpi" :delimiter ",")))
+      (:trap (stdout)))
+
+(defn diabetes-drugs [scrips]
+  (<- [?bnf-code ?bnf-name]
+      (scrips :#> 10 {3 ?bnf-code 4 ?bnf-name})
+      (diabetes-drug? ?bnf-name)
+      (:distinct true)))
+
+#_(?- (hfs-delimited "./output/diabetes-drugs" :delimiter "," :sinkmode :replace)
+      (diabetes-drugs
        (gp-prescriptions
         (hfs-delimited "./input/prescriptions/pdpi" :delimiter ",")))
       (:trap (stdout)))
